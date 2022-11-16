f016c3dde37ac035195b4840d5b414d61ac8fc10
Robert Andrici <andricirobby@gmail.com>
2022-11-16 15:00:48 +0200

    Working

--- customers/settings.py
+++ customers/settings.py
@@ -19,6 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-xw1bqc%qa#itr_uzo^^=(4io8xmq-@r$h#q0pz*3jw^ac6eks7'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -29,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'customers',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
